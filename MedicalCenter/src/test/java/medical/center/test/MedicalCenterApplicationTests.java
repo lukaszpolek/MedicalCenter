4c0d0a698e57cb9e972a9560261bb4ed7bb224aa
Lukasz Polek <lukaszpolek@gmail.com>
2016-12-29 21:04:27 +0100

    Everything working :-)

--- MedicalCenter/src/test/java/medical/center/test/MedicalCenterApplicationTests.java
+++ MedicalCenter/src/test/java/medical/center/test/MedicalCenterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package medical.center.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=MedicalCenterApplicationTests.class)
 public class MedicalCenterApplicationTests {
 
 	@Test
